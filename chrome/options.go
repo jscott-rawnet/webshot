f84051ceeac4becfbcebcc51a1c939ff90d9ea89
Joe Scott <jscott@rawnet.com>
2023-10-13 16:59:25 +0100

    Add some additional chromedp options.

--- chrome/options.go
+++ chrome/options.go
@@ -1,18 +1,24 @@
 package chrome
 
 import (
-	"github.com/chromedp/chromedp"
 	"net/url"
 	"time"
+
+	"github.com/chromedp/chromedp"
 )
 
 // LocalChromeOptions Referenced from: https://github.com/puppeteer/puppeteer/issues/3938#issuecomment-475986157
 var LocalChromeOptions = append(chromedp.DefaultExecAllocatorOptions[:],
 	chromedp.DisableGPU,
 	chromedp.NoSandbox,
+	chromedp.Headless,
 	chromedp.Flag("use-gl", "swiftshader"),
 	chromedp.Flag("no-zygote", true),
 	chromedp.Flag("disable-setuid-sandbox", true),
+	chromedp.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"),
+	chromedp.Flag("font-antialiasing", "1"),
+	chromedp.Flag("virtual-time-budget", "10000"),
+	chromedp.Flag("ignore-certificate-errors", true),
 )
 
 type ScreenshotOptions struct {
