3d566c22a416ff6d081c84086b6008555c55acff
Joe Scott <jscott@rawnet.com>
2023-10-13 17:01:32 +0100

    Improvements
    Add delay value to browser timeout.
    Handle alert boxes and browser crashes.
    Remove additional cookie/chat windows.
    Add some additional waits for page content.

--- chrome/chrome.go
+++ chrome/chrome.go
@@ -6,6 +6,9 @@ import (
 
 	"github.com/4everland/screenshot/lib"
 	"github.com/chromedp/cdproto/dom"
+	"github.com/chromedp/cdproto/inspector"
+	"github.com/chromedp/cdproto/page"
+	"github.com/chromedp/cdproto/runtime"
 	"github.com/chromedp/chromedp"
 )
 
@@ -31,18 +34,60 @@ func NewLocalChrome(execPath, proxy string) *Chrome {
 }
 
 func (c Chrome) Screenshot(parent context.Context, o ScreenshotOptions) (b []byte, err error) {
-	timeoutCtx, cancel := context.WithTimeout(parent, time.Until(o.EndTime))
-	defer cancel()
+	timeoutCtx, cancelTimeoutCtx := context.WithTimeout(parent, time.Until(o.EndTime.Add(o.Delay)))
+	defer cancelTimeoutCtx()
 
 	ctx, cancel := chromedp.NewContext(timeoutCtx)
 	defer cancel()
 
+	/* --- prevent browser crashes from locking the context (prevents hanging) --- */
+	chromedp.ListenTarget(ctx, func(ev interface{}) {
+		if _, ok := ev.(*inspector.EventTargetCrashed); ok {
+			cancel()
+		}
+	})
+
+	chromedp.ListenTarget(timeoutCtx, func(ev interface{}) {
+		if _, ok := ev.(*inspector.EventTargetCrashed); ok {
+			cancelTimeoutCtx()
+		}
+	})
+	/* --- */
+
+	/* --- squash JavaScript dialog boxes such as alert(); --- */
+	chromedp.ListenTarget(timeoutCtx, func(ev interface{}) {
+		if _, ok := ev.(*page.EventJavascriptDialogOpening); ok {
+			go func() {
+				if err := chromedp.Run(timeoutCtx,
+					page.HandleJavaScriptDialog(true),
+				); err != nil {
+					cancelTimeoutCtx()
+				}
+			}()
+		}
+	})
+	/* --- */
+
 	if err = chromedp.Run(ctx, chromedp.Tasks{
 		chromedp.EmulateViewport(o.Width, o.Height),
 		chromedp.Navigate(o.URL.String()),
+		chromedp.Evaluate(`document.readyState === 'complete'`, nil),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			_, exc, err := runtime.Evaluate(`document.fonts.ready`).WithAwaitPromise(true).Do(ctx)
+			if err != nil {
+				return err
+			}
+			if exc != nil {
+				return exc
+			}
+			return nil
+		}),
 		chromedp.Sleep(o.Delay),
 		chromedp.Evaluate("document.querySelector('.osano-cm-dialog')?.remove()", nil),
 		chromedp.Evaluate("document.querySelector('#onetrust-consent-sdk')?.remove()", nil),
+		chromedp.Evaluate("document.querySelector('#hs-eu-cookie-confirmation')?.remove()", nil),
+		chromedp.Evaluate("document.querySelector('#hubspot-messages-iframe-container')?.remove()", nil),
+
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			if o.Full {
 				return chromedp.FullScreenshot(&b, 100).Do(ctx)
